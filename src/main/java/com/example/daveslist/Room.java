a8c723c9f6021ea2f6ba57eac7827e4b37752cd9
Kim Levin <klevin5@student.umuc.edu>
2018-02-14 13:32:08 -0500

    Initial commit

---
+++ src/main/java/com/example/daveslist/Room.java
@@ -0,0 +1,148 @@
+package com.example.daveslist;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @NotEmpty
+    private String address;
+
+    @NotNull
+    @NotEmpty
+    private String city;
+
+    @NotNull
+    @NotEmpty
+    private String state;
+
+    @NotNull
+    @NotEmpty
+    private String price;
+
+    @NotNull
+    @NotEmpty
+    private String description;
+
+    @NotNull
+    @NotEmpty
+    private String rules;
+
+    @NotNull
+    @NotEmpty
+    private  boolean wifi;
+
+    @NotNull
+    @NotEmpty
+    private String cable;
+
+    @NotNull
+    @NotEmpty
+    private boolean bathroom;
+
+    @NotNull
+    @NotEmpty
+    private boolean isRented;
+
+    public Room() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRules() {
+        return rules;
+    }
+
+    public void setRules(String rules) {
+        this.rules = rules;
+    }
+
+    public boolean isWifi() {
+        return wifi;
+    }
+
+    public void setWifi(boolean wifi) {
+        this.wifi = wifi;
+    }
+
+    public String getCable() {
+        return cable;
+    }
+
+    public void setCable(String cable) {
+        this.cable = cable;
+    }
+
+    public boolean isBathroom() {
+        return bathroom;
+    }
+
+    public void setBathroom(boolean bathroom) {
+        this.bathroom = bathroom;
+    }
+
+    public boolean isRented() {
+        return isRented;
+    }
+
+    public void setRented(boolean rented) {
+        isRented = rented;
+    }
+}
